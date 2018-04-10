cc49063531cedc6f91414cbeeec207cf93d2b372
naveter <naveter@gmail.com>
2018-04-10 13:23:15 +0400

    SpringMyBatis init

---
+++ SpringMyBatis/src/main/java/learnspring/springmybatis/App.java
@@ -0,0 +1,15 @@
+package learnspring.springmybatis;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "spring-module.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+        obj.printHello();
+    }
+}
